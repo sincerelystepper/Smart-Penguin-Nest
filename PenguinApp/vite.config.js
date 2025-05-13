9b23c3e2ff2c651e003683eea3dc4292779a93d1
MHNNIC004 <mhnnic004@myuct.ac.za>
2025-05-13 23:11:27 +0200

    updated config default

--- PenguinApp/vite.config.js
+++ PenguinApp/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  server: {
+    host: '0.0.0.0', // Allows access from other devices on the same network
+    port: 5173,       // Default port, change it if needed
+  }
+};
