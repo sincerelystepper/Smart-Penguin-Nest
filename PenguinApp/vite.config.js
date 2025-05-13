8a0e4d7d8f8d56f692686845e13d9b54eb147714
MHNNIC004 <mhnnic004@myuct.ac.za>
2025-05-13 23:33:41 +0200

    Revert "updated config default"
    This reverts commit 9b23c3e2ff2c651e003683eea3dc4292779a93d1.

--- PenguinApp/vite.config.js
+++ PenguinApp/vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default {
-  server: {
-    host: '0.0.0.0', // Allows access from other devices on the same network
-    port: 5173,       // Default port, change it if needed
-  }
-};
+export default defineConfig({
+  plugins: [react()],
+})
