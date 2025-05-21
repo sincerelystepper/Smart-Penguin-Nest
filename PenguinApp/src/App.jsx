559e970abe13f1679d38127d77e289d12d6275d8
MHNNIC004 <mhnnic004@myuct.ac.za>
2025-05-21 21:36:51 +0200

    Updated RangeContext path

--- PenguinApp/src/App.jsx
+++ PenguinApp/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { RangeProvider } from './context/RangeContext';
+import { RangeProvider } from './RangeContext';
 
 // Import your pages (we'll create these next)
 import Home from './pages/Home.jsx';
