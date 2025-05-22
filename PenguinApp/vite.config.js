5a6bfa33113b5b50d579cbcf8ec51b06ad176332
MHNNIC004 <mhnnic004@myuct.ac.za>
2025-05-22 14:28:48 +0200

    Image optimization

--- PenguinApp/vite.config.js
+++ PenguinApp/vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+import { defineConfig } from 'vite'
+import imageOptimizer from 'vite-plugin-image-optimizer'
+
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [react(),
+  imageOptimizer({
+      png: {
+        quality: [0.6, 0.8],
+      },
+      jpeg: {
+        quality: 75,
+      },
+      webp: {
+        quality: 75,
+      },
+    })
+  ],
 })
